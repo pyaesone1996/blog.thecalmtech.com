abc04610254d368fbc01977173b6e977197383f8
Mr.Ps <pyaesone.march28@gmail.com>
2021-06-07 00:16:19 +0630

    firt commit

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link, graphql } from "gatsby" //highlight-line
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <SEO title="home" />
+      <h1>My WordPress Blog</h1>
+      <h4>Posts</h4>
+      {data.allWpPost.nodes.map(node => (
+        <div key={node.slug}>
+          {/* highlight-start */}
+          <Link to={node.slug}>
+            <p>{node.title}</p>
+          </Link>
+          {/* highlight-end */}
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }}/>
+          <p>{node.date}</p>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allWpPost(sort: { fields: [date] }) {
+      nodes {
+        title
+        excerpt
+        slug
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
